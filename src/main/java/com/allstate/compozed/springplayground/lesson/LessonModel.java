05d51eff1b2df90f99fe3b05d58feaa13b57af45
localadmin <localadmin@localadmins-iMac-2.local>
2017-07-18 11:00:36 -0500

    vinh and chad add db

--- src/main/java/com/allstate/compozed/springplayground/lesson/LessonModel.java
+++ src/main/java/com/allstate/compozed/springplayground/lesson/LessonModel.java
@@ -1,7 +1,44 @@
 package com.allstate.compozed.springplayground.lesson;
 
-/**
- * Created by localadmin on 7/18/17.
- */
-public class LessonModel {
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "lessons")
+final class LessonModel {
+
+    @Column(columnDefinition = "date")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date deliveredOn;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDeliveredOn() {
+        return deliveredOn;
+    }
+
+    public Date setDeliveredOn() {
+        return deliveredOn;
+    }
 }
